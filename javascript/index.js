88ecdaa6110bddfe61a7200ed82391526a65f71e
Gire1 <168452060+Gire1@users.noreply.github.com>
2026-01-14 15:04:36 +0200

    Added the moment time zone JS

--- javascript/index.js
+++ javascript/index.js
@@ -1,1 +1,25 @@
-alert("hello")
+function updateTime(){
+
+let losAgelesElement =document.querySelector("#los-angeles");
+let losAngelesDateElement = losAgelesElement.querySelector(".date");
+let losAngelesTimeElement =losAgelesElement.querySelector(".time");
+let losAngelesTime=moment().tz("America/Los_Angeles");
+
+losAngelesDateElement.innerHTML= moment().format("MMMM Do YYYY");
+losAngelesTmeElement.inneerHTML=losAngelesTimeElement.format("h:mm:ss [<small>P</small>]");
+
+
+
+let parisElement = document.querySelector("#paris");
+let parisDateElement = parisElement.querySelector(".date");
+let parisTimeElement = parisElement.querySelector(".time");
+let parisTme = moment().tz("Europe/Paris");
+
+parisDateElement.innerHTML = moment().format("MMMM Do YYYY");
+parisTmeElement.inneerHTML = parisTimeElement.format(
+  "h:mm:ss [<small>P</small>]"
+);
+
+updateTime();
+setInterval(updateTime, 1000);
+}
